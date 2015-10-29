78f5cbb2da4df66a5ddcb7283c875493e388932d
Will Chantry <goofiw@gmail.com>
2015-10-29 12:44:05 -0700

    deleted a post updates the view immediately

--- www/js/directives/settings-directive.js
+++ www/js/directives/settings-directive.js
@@ -14,24 +14,28 @@ angular.module('stuffmobile')
       '$ionicPopup', '$state', '$scope', function($ionicPopup, $state, $scope){
         $scope.remove = function() {
           $scope.post.remove().then(function(data) {
+            $scope.post.status = 'deleted';
             $ionicPopup.alert({title: 'notice', template: 'Post has been deleted'});
           })
         }
 
         $scope.undib = function() {
           $scope.post.undib().then(function(data) {
+            $post.status = 'new';
             $ionicPopup.alert({title: 'notice', template: 'Post has been undibbed'});
           })
         }
 
         $scope.gone = function() {
           $scope.post.markGone().then(function(data) {
+            $scope.ipost.status = 'gone';
             $ionicPopup.alert({title: 'notice', template: 'Post has been marked as picked up'});
           })
         }
 
         $scope.reject = function() {
           $scope.post.rejectDibber().then(function(data) {
+            $scope.post.status = 'new';
             $ionicPopup.alert({title: 'notice', template: 'Dib rejected'});
           })
         }
