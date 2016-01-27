8e32f79775b8e2a942ccee2f67cf39ba3f7f33a7
Will Chantry <goofiw@gmail.com>
2016-01-26 18:29:57 -0800

    added accessory bar

--- www/js/app.js
+++ www/js/app.js
@@ -13,9 +13,9 @@ angular.module('stuffmobile', ['ionic', 'ngCordova', 'ngResource', 'ngIOS9UIWebV
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
+    // if(window.cordova && window.cordova.plugins.Keyboard) {
+    //   cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    // }
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
